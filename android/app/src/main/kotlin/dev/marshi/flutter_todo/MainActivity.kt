03cda8cae11f8da2b4b9298e18ee137cf1e816aa
marshi <mashi3648@gmail.como>
2020-11-03 17:24:53 +0900

    initial commit.

---
+++ android/app/src/main/kotlin/dev/marshi/flutter_todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.marshi.flutter_todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_todo/home.dart';
+import 'package:flutter_todo/task_add/task_add_page.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  "/": (BuildContext context) => MyHomePage(title: 'Flutter Demo Home Page'),
+  "/add": (BuildContext context) => TaskAddPage(),
+};
